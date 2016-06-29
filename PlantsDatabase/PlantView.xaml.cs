1e461fd4c4a9480d9129afc9888cec4bd20359ee
Chris Prentice <christopher.prentice@advancedcomputersoftware.com>
2016-06-29 23:22:06 +0100

    refactored datacontext

---
+++ PlantsDatabase/PlantView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PlantsDatabase
+{
+    public partial class PlantView : Window
+    {
+        public PlantView()
+        {
+            InitializeComponent();
+        }
+    }
+}
